d25c77b9c9e72e2102b883ff812300b663753526
Dvid Silva <contacto@dvidsilva.com>
2013-03-30 03:17:32 -0500

    problem 4

--- 4 palindrome.php
+++ 4 palindrome.php
@@ -1,9 +1,16 @@
 <?
 
+$mtime = microtime(); 
+$mtime = explode(" ",$mtime); 
+$mtime = $mtime[1] + $mtime[0]; 
+$tstart = $mtime; 
+
+
 $p = 998001; #largest number made of multiplying 2 three digits numbers
 
 
 function palindrome( $r ){
+    /* Found a better solution
     $r = str_split($r);
     if(count($r)<6){
       return(false);
@@ -11,6 +18,11 @@ function palindrome( $r ){
     if( $r[0] == $r[5] && $r[1] == $r[4] && $r[2] == $r[3]){
         return(true);
     }
+    */
+    if($r == strrev($r) ){
+      return(true);
+    }
+    
 }
 
 $r = 1;
@@ -32,3 +44,11 @@ for($i = 100; $i <= 999; $i++){
 }
 $m = max($l);
 echo "$m $h H $i $j \n";
+
+
+$mtime = microtime(); 
+$mtime = explode(" ",$mtime); 
+$mtime = $mtime[1] + $mtime[0]; 
+$tend = $mtime; 
+$total = ($tend - $tstart); 
+echo "$total \n";
