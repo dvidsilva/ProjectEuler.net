4520a80c3fdedca3e755fb673cb26712eb97a878
Dvid Silva <contacto@dvidsilva.com>
2013-03-30 00:56:58 -0500

    4 solved

--- 4 palindrome.php
+++ 4 palindrome.php
@@ -1,33 +1,34 @@
 <?
-include('primenumbers.php');
+
+$p = 998001; #largest number made of multiplying 2 three digits numbers
 
 
-$p = 600851475143; #13195;
-
-
-function something( $r = array() ){
-    global $ar;
-    foreach($ar as $k){
-        $d = ($r[0] / $k);
-        echo "$d \n";
-        if( is_int($d) ){
-            echo "$d , $k \n";        
-            return([$d,$k]);
-        }
+function palindrome( $r ){
+    $r = str_split($r);
+    if(count($r)<6){
+      return(false);
     }
-    return($r);
+    if( $r[0] == $r[5] && $r[1] == $r[4] && $r[2] == $r[3]){
+        return(true);
+    }
 }
-$d = array($p , 2);
-$l = array();
+
+$r = 1;
 $i = 1;
-while( $d[0] != 1 ){
-    $d = something($d);
-    $l[] = $d;
-    $i++;
+$j = 1;
+$h = 0;
+$l = array();
+
+for($i = 100; $i <= 999; $i++){
+    for($j = 100; $j <= 999; $j++){
+        $r = $i * $j;
+        if( palindrome($r) ){
+            $l[] = $r;
+            echo "$r \n";
+        }
+        $h++;
+        
+    }
 }
-
-print_r($l);
-
-
-
-
+$m = max($l);
+echo "$m $h H $i $j \n";
